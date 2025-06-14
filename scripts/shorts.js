c2dd4481f33c733bb745a68247c82d407c054056
embi-3 <139082906+embi-3@users.noreply.github.com>
2025-06-14 21:32:35 +1000

    Fix formatting and clean up old comments

--- scripts/shorts.js
+++ scripts/shorts.js
@@ -1,8 +1,6 @@
 const CHECK_INTERVAL = 20
 
 function removePopUps() {
-    // let shorts = document.querySelectorAll("div#content.style-scope.ytd-rich-section-renderer");
-    // let popups = document.querySelectorAll("div#dismissible.style-scope.ytd-rich-shelf-renderer");
     let popups = document.querySelectorAll("ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer");
     
     if (popups.length != 0) {
@@ -40,28 +38,6 @@ function removeReelShelf() {
     }
 }
 
-// ! DEBUG FUNCTION
-function createIndicator() {
-    const app = document.querySelector("ytd-app");
-    const content = document.querySelector("div#content.style-scope.ytd-app");
-
-    const indicator = document.createElement("div");
-    indicator.id = "content";
-    indicator.style = "font-size:32px; background-color:white; text-align:center; padding:10px"
-    const text = document.createElement("p");
-    text.textContent = "Minimal YT is active!";
-    indicator.appendChild(text);
-
-    const masthead = document.querySelector("div#masthead-container.style-scope.ytd-app");
-    masthead.style = "margin-top:58.55px";
-
-    const sidebar = document.querySelector("ytd-mini-guide-renderer");
-    sidebar.style = "margin-top:58.55px";
-    // masthead.insertAdjacentElement("afterend", indicator);
-
-    app.insertBefore(indicator, content);
-}
-
 function logger(records, observer) {
     for (const record of records) {
         if (record.addedNodes) {
@@ -82,19 +58,7 @@ function editPage() {
     removeReelShelf();
 }
 
-function createObserver(body) {    
-    // let body = document.querySelector("div#content");
-    // console.log("body:", body);
-
-    // const browse = document.querySelector("ytd-browse");
-    // console.log(browse);
-
-    // const columns = document.querySelector("ytd-two-column-browse-results-renderer");
-    // console.log(columns);
-
-    // const content = document.querySelector("div#contents.style-scope.ytd-rich-grid-renderer"); // not seen at runtime
-    // console.log(content);
-
+function createObserver(body) {
     let observer = new MutationObserver(logger);
 
     const observerOptions = {
@@ -117,17 +81,14 @@ if (window.hasRun === true) {
     console.log("Already running!");
 } else {
     window.hasRun = true;
-    console.log("Now running!")
+    console.log("Now running!");
 
-    // Checks whether the body element has been created yet.
+    // Periodically check whether the content has loaded yet.
     // See: https://stackoverflow.com/questions/42731323/content-script-not-executing-in-new-window
     if (document.querySelector("div#content")) {
         let body = document.querySelector("div#content")
         createObserver(body)
     } else {
-        // Find a good interval to check for the body
         var interval = setInterval(checkContent, CHECK_INTERVAL);
     }
-
-    // createIndicator();
 }
